99371bb7695b2d5f755195125e833d42f1177fd0
Petrequin Simon <simon.petrequin@etudiant.univ-rennes1.fr>
2024-08-11 18:10:11 +0200

    initial commit

---
+++ src/client/java/afrocraft/afrocraft/AfroCraftClient.java
@@ -0,0 +1,10 @@
+package afrocraft.afrocraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AfroCraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
