d1f87c09dbffb7e9b1ccc05979de00c888cae84e
Saunier Martin <martin.saunier35@gmail.com>
2024-08-11 22:23:34 +0200

    Added textures and models

--- src/client/java/afrocraft/afrocraft/AfroCraftClient.java
+++ src/client/java/afrocraft/afrocraft/AfroCraftClient.java
@@ -1,10 +1,17 @@
 package afrocraft.afrocraft;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+
+import static afrocraft.afrocraft.block.ModBlocks.WEED_CROP;
 
 public class AfroCraftClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+		// allow this block to have transparent cutouts
+		BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), WEED_CROP);
 	}
 }
