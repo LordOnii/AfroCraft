41bdd505b7c7c568fcdec465e74d3ef5973900d1
Saunier Martin <martin.saunier35@gmail.com>
2024-08-11 22:13:11 +0200

    Added WeedCrop voxelShape by age

--- src/main/java/afrocraft/afrocraft/block/custom/WeedCrop.java
+++ src/main/java/afrocraft/afrocraft/block/custom/WeedCrop.java
@@ -5,15 +5,28 @@ import afrocraft.afrocraft.block.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 
 public class WeedCrop extends CropBlock {
 
     public static final int MAX_AGE = 5;
     public static final IntProperty AGE = Properties.AGE_5;
+
+    private static final VoxelShape[] AGE_TO_SHAPE = new VoxelShape[]{
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 2.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 4.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 6.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 8.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 10.0, 16.0),
+            Block.createCuboidShape(0.0, 0.0, 0.0, 16.0, 10.0, 16.0)
+    };
 
     public WeedCrop(Settings settings) {
         super(settings);
@@ -32,6 +45,11 @@ public class WeedCrop extends CropBlock {
     @Override
     protected IntProperty getAgeProperty() {
         return AGE;
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return AGE_TO_SHAPE[(Integer)state.get(this.getAgeProperty())];
     }
 
     @Override
