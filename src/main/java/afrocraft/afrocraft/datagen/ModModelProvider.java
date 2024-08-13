c1034f6a1732237e8a354846bc3bf1a532149707
Saunier Martin <martin.saunier35@gmail.com>
2024-08-13 02:58:48 +0200

    Ajout des methodes et extends de ModItemTagProvider, ModModelProvider and ModRecipeProvider

--- src/main/java/afrocraft/afrocraft/datagen/ModModelProvider.java
+++ src/main/java/afrocraft/afrocraft/datagen/ModModelProvider.java
@@ -1,4 +1,31 @@
 package afrocraft.afrocraft.datagen;
 
-public class ModModelProvider {
+import afrocraft.afrocraft.block.ModBlocks;
+import afrocraft.afrocraft.block.custom.WeedCrop;
+import afrocraft.afrocraft.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+
+public class ModModelProvider extends FabricModelProvider {
+
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        blockStateModelGenerator.registerCrop(ModBlocks.WEED_CROP, WeedCrop.AGE, 0, 1, 2, 3, 3, 5);
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        itemModelGenerator.register(ModItems.BLUNT, Models.GENERATED);
+        itemModelGenerator.register(ModItems.WEED , Models.GENERATED);
+        itemModelGenerator.register(ModItems.WEED_SEEDS , Models.GENERATED);
+        itemModelGenerator.register(ModItems.BAKED_CARROT , Models.GENERATED);
+
+    }
 }
--- src/main/java/afrocraft/afrocraft/datagen/ModRecipeProvider.java
+++ src/main/java/afrocraft/afrocraft/datagen/ModRecipeProvider.java
@@ -1,4 +1,37 @@
 package afrocraft.afrocraft.datagen;
 
-public class ModRecipeProvider {
+import afrocraft.afrocraft.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.RecipeProvider;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+
+    public ModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    public void generate(RecipeExporter exporter) {
+        offerSmelting(exporter, List.of(Items.CARROT), RecipeCategory.FOOD, ModItems.BAKED_CARROT, 0.7f, 200, "food");
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.BLUNT)
+                .pattern("WWW")
+                .pattern("PPP")
+                .input('W', ModItems.WEED)
+                .input('P', Items.PAPER)
+                .criterion(hasItem(ModItems.WEED), conditionsFromItem(ModItems.WEED))
+                .criterion(hasItem(Items.PAPER), conditionsFromItem(Items.PAPER))
+                .offerTo(exporter);
+    }
 }
